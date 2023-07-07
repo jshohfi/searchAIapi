b823d877bf3afb969f55a07b04f6ba003773bfbb
james shohfi <jshohfi@gmail.com>
2023-07-06 23:08:19 -0700

    deleted makechain.ts, incorporated its code into index.js

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const OpenAIEmbeddings = require('langchain/embeddings/openai').OpenAIEmbeddings
 
 // +jls+ 6-July-2023 makechain.ts and pinecone-client.ts 
 // +jls+ 6-July-2023 from gptPdfLangChainPinecone
-const makeChain = require('./makechain');
-const pinecone = require('./pinecone-client');
+// +jls+ 6-July-2023 imports in makechain.ts
+const OpenAI = require('langchain/llms/openai');
 const PineconeStore = require('langchain/vectorstores/pinecone');
+const ConversationalRetrievalQAChain = require('langchain/chains');
+// const makeChain = require('./makechain');
+const pinecone = require('./pinecone-client');
 
 const {Configuration, OpenAIApi} = require('openai');
 const express = require('express');
@@ -31,6 +34,41 @@ const config = new Configuration({
 });
 
 const openai = new OpenAIApi(config);
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0, 
+    modelName: 'gpt-4',
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, // default to return 4 source docs
+    },
+  );
+  return chain;
+};
+
 
 // +js+ 6-July-2023 comment out -- change homemade to LangChain
 // define initPinecone function to return pinecone
