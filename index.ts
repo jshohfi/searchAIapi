e13cd9dde0c1d7b3fdf52a66b58a595638cc109a
james shohfi <jshohfi@gmail.com>
2023-07-11 14:48:52 -0700

    First deployment of LangChain blackbox docs handling

--- index.ts
+++ index.ts
@@ -1,18 +1,32 @@
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? '';
-const PINECONE_API_KEY = process.env.PINECONE_API_KEY ?? '';
-const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT ?? '';
-const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
-const PineconeClient = require('@pinecone-database/pinecone').PineconeClient;
-const OpenAIEmbeddings = require('langchain/embeddings/openai').OpenAIEmbeddings;
+const OPENAI_API_KEY = process.env['OPENAI_API_KEY'] ?? '';
+const PINECONE_API_KEY = process.env['PINECONE_API_KEY'] ?? '';
+const PINECONE_ENVIRONMENT = process.env['PINECONE_ENVIRONMENT'] ?? '';
+const PINECONE_INDEX_NAME = process.env['PINECONE_INDEX_NAME'] ?? '';
 
-// +jls+ 6-July-2023 makechain.ts and pinecone-client.ts 
+// +jls+ 7-July-2023 import instead of require 
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+// const OpenAIEmbeddings = require('langchain/embeddings/openai').OpenAIEmbeddings;
+
+// +jls+ 7-July-2023 import instead of require 
+// +jls+ 7-July-2023 importing makechain.ts and pinecone-client.ts 
 // +jls+ 6-July-2023 from gptPdfLangChainPinecone
 // +jls+ 6-July-2023 imports in makechain.ts
-const OpenAI = require('langchain/llms/openai');
-const PineconeStore = require('langchain/vectorstores/pinecone');
-const ConversationalRetrievalQAChain = require('langchain/chains');
-// const makeChain = require('./makechain');
-// const pinecone = require('./pinecone-client');
+// const OpenAI = require('langchain/llms/openai');
+
+
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { makeChain } from './utils/makechain';
+
+// +jls+ we'll incorporate pinecone-client.ts code in index.ts
+import { PineconeClient } from '@pinecone-database/pinecone';
+// import { pinecone } from './utils/pinecone-client';
+
+// const PineconeStore = require('langchain/vectorstores/pinecone');
+// const makeChain = require('./utils/makechain');
+// const pinecone = require('./utils/pinecone-client');
+
+// +jls+ 7-July-2023 No, we'll be getting namespace in the post
+// const PINECONE_NAME_SPACE = require('./config/pinecone');
 
 const {Configuration, OpenAIApi} = require('openai');
 const express = require('express');
@@ -30,98 +44,19 @@ app.use(cors());
 // var indexResponse = {};
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env['OPENAI_API_KEY']
 });
 
 const openai = new OpenAIApi(config);
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:`;
-
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
-const makeChain = (vectorstore: typeof PineconeStore) => {
-  const model = new OpenAI({
-    temperature: 0,
-    modelName: 'gpt-4',
-  });
-// const makeChain = (vectorstore) => {
-//   const model = new OpenAI({
-//     temperature: 0, 
-//     modelName: 'gpt-4',
-//   });
-
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorstore.asRetriever(),
-    {
-      qaTemplate: QA_PROMPT,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, // default to return 4 source docs
-    },
-  );
-  return chain;
-};
-
-
-// +js+ 6-July-2023 comment out -- change homemade to LangChain
-// define initPinecone function to return pinecone
-async function initPinecone() {
-  try {
-    const pinecone = new PineconeClient();
-    await pinecone.init({
-        environment: PINECONE_ENVIRONMENT,
-        apiKey: PINECONE_API_KEY
-    });
-    return pinecone;
-  } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to initialize Pinecone Client');
-  }
-}
-
-// +js+ 6-July-2023 comment out -- change homemade to LangChain
-// async function queryPinecone(uniPrompt, nameSpace) {
-//     const pinecone = await initPinecone();
-//     const embeddings = new OpenAIEmbeddings({
-//         openAIApiKey: OPENAI_API_KEY,
-//     });
-//     const embeddedQuery = await embeddings.embedQuery(uniPrompt);
-//     const index = pinecone.Index(PINECONE_INDEX_NAME);
-//     const queryRequest = {
-//         "topK": 3,
-//         "vector": embeddedQuery,
-//         "includeMetadata": true,
-//         "includeValues": true,
-//         "namespace": nameSpace
-//     }
-//     const queryResponse = await index.query({queryRequest});
-//     return (queryResponse);
-// }  
-
-app.get('/', async (req, res) => {
+app.get('/', async (req: any, res: any) => {
   res.status(200).send({
     message: 'Hello from searchAIapi'
   })
 })
 
-app.post('/', async (req, res) => {
+app.post('/', async (req: any, res: any) => {
   try {
-
-    // +jls+ 6-July-2023 not sure where or when to invoke this:
-    const pinecone = await initPinecone();
-
     // +jls+ 30-June-2023 searchAI individual bots optionally specify a model ID,
     //    else searchAI/bot-io.services.ts sets default model to gpt-3.5-turbo
     // +jls+ 4-July-2023 they also optional specify temperature (0 to 2, def .3),
@@ -170,7 +105,7 @@ app.post('/', async (req, res) => {
       // indexResponse = await queryPinecone(uniprompt, namespace);
       //
       // +js+ 6-July-2023 get expertise from chatMessages[0]
-      const expertise = chatMessages[0].content;
+      const expertise: string = chatMessages[0].content;
       // +js+ 6-July-2023 get current question from final element
       const prompt_text = chatMessages[chatMessages.length - 1].content;
       const sanitizedQuestion = prompt_text.trim().replaceAll('\n', ' ');
@@ -180,21 +115,44 @@ app.post('/', async (req, res) => {
       try {
 
         // +jls+ 7-July-2023 per https://js.langchain.com/docs/modules/indexes/vector_stores/integrations/pinecone
-        const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+        // +jls+ 11-July-2023 check our access to env constants
+        // console.log("[OPENAI_API_KEY]=" + process.env['OPENAI_API_KEY']);
+        // console.log("[PINECONE_API_KEY]=" + process.env['PINECONE_API_KEY']);
+        // console.log("[PINECONE_ENVIRONMENT]=" + process.env['PINECONE_ENVIRONMENT']);
+        // console.log("[PINECONE_INDEX_NAME]=" + process.env['PINECONE_INDEX_NAME']);
+
+        // +jls+ 7-July-2023 we'll init pinecone inline instead
+        const pinecone = new PineconeClient();
+        await pinecone.init({
+            environment: process.env['PINECONE_ENVIRONMENT']!,
+            apiKey: process.env['PINECONE_API_KEY']!,
+        });
+        // const pinecone = await initPinecone();
+
+        const index = pinecone.Index(process.env['PINECONE_INDEX_NAME']!);
+
+        /* create vectorstore*/
         const vectorStore = await PineconeStore.fromExistingIndex(
-          index,
-          new OpenAIEmbeddings({}),
-          'text',
-          PINECONE_NAME_SPACE, 
+            new OpenAIEmbeddings({}),
+            {
+            pineconeIndex: index,
+            textKey: 'text',
+            namespace: 'Higher Education Act of 1965',
+            },
         );
-        // const index = pinecone.Index(PINECONE_INDEX_NAME);
         // const vectorStore = await PineconeStore.fromExistingIndex(
+        //   index,
         //   new OpenAIEmbeddings({}),
-        //   {
-        //     pineconeIndex: index,
-        //     textKey: 'text',
-        //     namespace: PINECONE_NAME_SPACE,
-        //   },
+        //   'text',
+        //   PINECONE_NAME_SPACE, 
+        // );
+        // const vectorStore = await PineconeStore.fromExistingIndex(
+        //     new OpenAIEmbeddings(),
+        //     { 
+        //       pineconeIndex: index,
+        //       namespace: process.env.PINECONE_NAMESPACE ?? ''
+        //     }
         // );
     
         // console.log("vectorStore.asRetriever()=" + JSON.stringify(vectorStore.asRetriever()));
@@ -206,17 +164,56 @@ app.post('/', async (req, res) => {
           question: sanitizedQuestion,
           chat_history: historyQA || [],
         });
-        botResponse = response;
-        console.log('**botResponse=', botResponse);
-        console.log('**response.sourceDocuments[0].metadata.source=', response.sourceDocuments[0].metadata.source);
     
-        res.status(200).json(response);
-    
-      } catch (error) {
-        console.log('error', error);
-        res.status(500).json({ error: error.message || 'Something went wrong' });
-      }
+        // +js+ 10-July-2023 change homemade to LangChain
+        // +jls+ 10--July-2023 process the four custom doc citations
+        let filePath, fileNameExt = '';
+        let docExcerpt1, docExcerpt2, docExcerpt3, docExcerpt4 = '';
 
+        filePath = response['sourceDocuments'][0].metadata.source
+        fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
+        docExcerpt1 = 'Source 1: ' + response['sourceDocuments'][0].pageContent 
+        + '<br>—' + fileNameExt.trim();
+
+        filePath = response['sourceDocuments'][1].metadata.source
+        fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
+        docExcerpt2 = 'Source 2: ' + response['sourceDocuments'][1].pageContent 
+        + '<br>—' + fileNameExt.trim();
+
+        filePath = response['sourceDocuments'][2].metadata.source
+        fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
+        docExcerpt3 = 'Source 3: ' + response['sourceDocuments'][2].pageContent 
+        + '<br>—' + fileNameExt.trim();
+
+        filePath = response['sourceDocuments'][3].metadata.source
+        fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
+        docExcerpt4 = 'Source 4: ' + response['sourceDocuments'][3].pageContent 
+        + '<br>—' + fileNameExt.trim();
+    
+        // +js+ 10-July-2023 change homemade to LangChain
+        // res.status(200).json(response);
+        botResponse = response['text'];
+        res.status(200).send({
+            bot: botResponse,
+            doc1: docExcerpt1,
+            doc2: docExcerpt2,
+            doc3: docExcerpt3,
+            doc4: docExcerpt4
+          });
+        // console.log('**botResponse=', botResponse);
+        // console.log("**response['sourceDocuments'][0].pageContent=", 
+        //     response['sourceDocuments'][0].pageContent);
+
+      } catch (error) {
+        // +jls+ 8-July-2023
+        let errorMessage = 'Something went wrong';
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        console.log(errorMessage);
+        // console.log('error', error);
+        // res.status(500).json({ error: error.message || 'Something went wrong' });
+      }
     }
 
     // +jls+ 17-June-2023 only do this below if no custom docs
@@ -226,51 +223,31 @@ app.post('/', async (req, res) => {
       // +jls+ 4-July-2023 they also optional specify temperature (0 to 2, def .3),
       //   max_tokens (8192 gpt-4, def 4096), frequency_penalty (-2 to 2, def .3)
       // +jls+ 16-June-2023 chatMessages[] has messages array (with retrieved doc excerpts in final "user" element's content if namespace)
+
+      // +jls+ 7-July-2023 openai may not like params or need updating, i don't know
       const response = await openai.createChatCompletion({
         model: model,
-        max_tokens: max_tokens,
         messages: chatMessages,
-        temperature: temperature,
-        frequency_penalty: frequency_penalty
       });
+      // const response = await openai.createChatCompletion({
+      //   model: model,
+      //   max_tokens: max_tokens,
+      //   messages: chatMessages,
+      //   temperature: temperature,
+      //   frequency_penalty: frequency_penalty
+      // });
 
       // +js+ 6-July-2023 per change homemade to LangChain
       botResponse = response.data.choices[0].message.content.trimStart();
       // let botResponse = response.data.choices[0].message.content.trimStart();
+    
+      // +js+ 10-July-2023 only send this here if namespace is 'none'
+      res.status(200).send({
+        bot: botResponse,
+      });
     }
     // +jls+ 17-June-2023 only do stuff above if no custom docs
 
-    // +js+ 6-July-2023 comment out -- change homemade to LangChain
-    // +jls+ 26-June-2023 trim, add <br> and dash before filename in docExcerpts
-    // +jls+ 20-June-2023 only filename.ext (not whole path) in docExcerpt*
-    // +jls+ 17-June-2023 if docs to search, process the three citations
-    // let filePath, fileNameExt, docExcerpt1, docExcerpt2, docExcerpt3 = '';
-    // if (namespace != 'none') {
-    //   filePath = indexResponse.matches[0].metadata.source
-    //   fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
-    //   docExcerpt1 = 'Source 1: ' + indexResponse.matches[0].metadata.text 
-    //   + '<br>—' + fileNameExt.trim();
-    //   filePath = indexResponse.matches[1].metadata.source
-    //   fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
-    //   docExcerpt2 = 'Source 2: ' + indexResponse.matches[1].metadata.text 
-    //   + '<br>—' + fileNameExt.trim();
-    //   filePath = indexResponse.matches[2].metadata.source
-    //   fileNameExt = filePath.substring(filePath.lastIndexOf('\\') + 1);
-    //   docExcerpt3 = 'Source 3: ' + indexResponse.matches[2].metadata.text 
-    //   + '<br>—' + fileNameExt.trim();
-    // }  
-
-    // +js+ 6-July-2023 comment out -- change homemade to LangChain
-    res.status(200).send({
-      bot: botResponse,
-    });
-    // res.status(200).send({
-    //   bot: botResponse,
-    //   doc1: docExcerpt1,
-    //   doc2: docExcerpt2,
-    //   doc3: docExcerpt3
-    // });
-  
   } catch (error) {
       console.error(error)
       res.status(500).send(error || 'Something went wrong');
@@ -280,3 +257,21 @@ app.post('/', async (req, res) => {
 app.listen(44444, () => {
     console.log('searchAIapi listening on port 44444');
 });
+
+//**************************************************************
+// +jls+ 10-July-2023 below cloned from pinecone-client.ts
+async function initPinecone() {
+    try {
+      const pinecone = new PineconeClient();
+      await pinecone.init({
+        environment: process.env['PINECONE_ENVIRONMENT'] ?? '',
+        apiKey: process.env['PINECONE_API_KEY'] ?? '',
+      });
+      return pinecone;
+    } catch (error) {
+      console.log('error', error);
+      throw new Error('Failed to initialize Pinecone Client');
+    }
+}
+// +jls+ 10-July-2023 above cloned from pinecone-client.ts
+//**************************************************************
